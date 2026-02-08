b0b667bc630510a57d29749bfb86e1305cd1f535
eepyweeepy <eepy@gmail.com>
2026-02-08 10:19:24 +0200

    init

---
+++ index_files/scripts.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    InitUI();
+});
+
+function InitUI() {
+    $.datepicker.setDefaults($.datepicker.regional[language]);
+    $(".date-picker").datepicker({ changeMonth: true, changeYear: true, showAnim: "fold", showOtherMonths: true, showWeek: true });
+}
